01a5c6bdaf4f5f828349bf95fe74b273ad5252b6
alexeybudyak <Alex42Olga>
2021-02-27 17:45:16 -0800

    shortest todo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [todo,setTodo] = useState([{id: 1, name: 'Eat', done: false},
+                                             {id: 2, name: 'Pray', done: false},
+                                             {id: 3, name: 'Love', done: false},
+                                             {id: 4, name: 'Dance', done: false}])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        {
+          todo.map((el, i) => <div key={el.id}>
+            {el.done ? <s>{el.name}</s> : el.name}
+            <button onClick={() => {
+              todo[i].done = !todo[i].done;
+              setTodo([...todo])
+            }}>  {el.done ? 'Undone': 'Done'} </button>
+            <button onClick={() => {
+              setTodo(todo.filter(e=>e.id!==el.id))
+            }}> Delete </button>
+            {i>1 && <button onClick={() =>{
+              [todo[i],todo[i-1]] = [todo[i-1],todo[i]];
+              setTodo([...todo])
+            }}> ^ </button>}
+            {i<todo.length-1 && <button onClick={() =>{
+              [todo[i],todo[i+1]] = [todo[i+1],todo[i]];
+              setTodo([...todo])
+            }}> v </button>}
+          </div>)
+        }
+      <input type="text" id="newTask"/>
+      <button onClick={() => {
+        setTodo([...todo, {id: Math.random(), name: document.getElementById("newTask").value}])
+        document.getElementById("newTask").value = '';
+      }}> Add </button>
     </div>
   );
 }
